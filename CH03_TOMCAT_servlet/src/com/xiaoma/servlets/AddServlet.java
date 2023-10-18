95695dca0676c41b59b0487e799f41cfa3763233
MaBuyao <860977475@qq.com>
2023-10-18 09:36:43 +0800

    mby1018

---
+++ CH03_TOMCAT_servlet/src/com/xiaoma/servlets/AddServlet.java
@@ -0,0 +1,23 @@
+package com.xiaoma.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * ClassName: AddServlet
+ * Description:
+ *
+ * @Author Mabuyao
+ * @Create 2023/10/17 17:47
+ * @Version 1.0
+ */
+public class AddServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest requset, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
